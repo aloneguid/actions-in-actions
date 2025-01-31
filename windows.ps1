7870440a12a907a1747fbd8420f32ef7fd825682
IG <aloneguid@outlook.com>
2025-01-31 12:37:09 +0000

    for the win

---
+++ windows.ps1
@@ -0,0 +1,84 @@
+function Take-Screenshot {
+    param (
+        [string]$FileName
+    )
+
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    $bounds = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $bitmap = New-Object Drawing.Bitmap $bounds.Width, $bounds.Height
+    $graphics = [Drawing.Graphics]::FromImage($bitmap)
+    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.Size)
+    $bitmap.Save($FileName, [Drawing.Imaging.ImageFormat]::Png)
+}
+
+# Example usage:
+# Take-Screenshot -FileName "screenshot.png"
+
+function Set-ScreenResolution {
+    param (
+        [int]$Width = 3840,
+        [int]$Height = 2160
+    )
+
+    Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class User32 {
+        [DllImport("user32.dll")]
+        public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE {
+            public const int CCHDEVICENAME = 32;
+            public const int CCHFORMNAME = 32;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public short dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+    }
+"@
+
+    $devMode = New-Object User32+DEVMODE
+    $devMode.dmSize = [System.Runtime.InteropServices.Marshal]::SizeOf($devMode)
+    $devMode.dmPelsWidth = $Width
+    $devMode.dmPelsHeight = $Height
+    $devMode.dmFields = 0x80000 | 0x100000
+    [User32]::ChangeDisplaySettings([ref]$devMode, 0)
+}
+
+# Example usage:
+# Set-ScreenResolution -Width 3840 -Height 2160
+
+Take-Screenshot -FileName "init.png"
+Set-ScreenResolution
+Take-Screenshot -FileName "4k.png"
